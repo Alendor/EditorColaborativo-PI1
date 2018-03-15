d0acbeb28adb84fbfd6c4dcfd56b1697e4ebc7c0
sebastian.osorno <osorno.sebastian@gmail.com>
2018-03-15 18:28:59 -0500

    minuto 33

--- public/main.js
+++ public/main.js
@@ -1,4 +1,29 @@
-var obj={
-    "start": "node main.js"
-  };
-obj  
+var socket = io.connect('http://localhost:8084',
+    {'forceNew': true});
+
+ socket.on('messages', function(data){
+
+    console.log(data);   
+    render(data);   
+})    
+
+function render(data){
+    var html = data.map(function(elem, index){
+        return(`<div>
+        <strong>${elem.autor}</strong>:
+        <em>${elem.text}</em></div>`);
+    }).join(" ");
+
+    document.getElementById('messages').innerHTML = html;
+}
+
+function addMessage(e){
+    var payload = {
+        author: document.getElementById(username).value,
+        text: document.getElementById(texto).value
+    };
+
+    socket.emit('new-message',payload);
+    return false;
+
+}
--- server/main.js
+++ server/main.js
@@ -1,3 +1,32 @@
 var express = require('express');
 var app = express();
 var server = require('http').Server(app);
+var io = require('socket.io')(server);
+var messages =[{
+    id: 1,
+    text: "Hola soy mensaje",
+    autor: "Sebastian O"
+}]
+
+app.use(express.static('public'));
+
+app.get('/', function(req, res){
+    res.status(200).send("hola mundo");
+ 
+});
+  
+io.on('connection', function(socket){
+    
+    console.log("Alguien se ha conectado!");
+    socket.emit('messages', messages);
+    socket.on('new-message', function(data){
+        messages.push(data);
+    }); 
+
+});      
+ 
+server.listen(8084, function(){
+
+    console.log("servidor corriendo  8084")
+});         
+ 
