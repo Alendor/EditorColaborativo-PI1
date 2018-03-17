73ab926c1791b279387d6ab6046f070c82fe31f7
sebastian.osorno <osorno.sebastian@gmail.com>
2018-03-17 17:51:22 -0500

    Lectura Json

--- public/main.js
+++ public/main.js
@@ -1,26 +1,28 @@
-var socket = io.connect('http://localhost:8084',
+var socket = io.connect(
     {'forceNew': true});
 
  socket.on('messages', function(data){
 
-    console.log(data);   
-    render(data);   
+    //console.log(data);   
+    render(data.value);   
 })    
 
 function render(data){
     var html = data.map(function(elem, index){
-        return(`<div>
-        <strong>${elem.autor}</strong>:
-        <em>${elem.text}</em></div>`);
+        return(`<div>   
+         <strong>${elem}</strong>:
+         <em>${elem.nombre_obra}</em>
+        </div>`);
     }).join(" ");
-
+    console.log(data);
     document.getElementById('messages').innerHTML = html;
 }
 
 function addMessage(e){
     var payload = {
-        author: document.getElementById(username).value,
-        text: document.getElementById(texto).value
+        author: document.getElementById('username').value,
+        text: document.getElementById('texto').value
+    
     };
 
     socket.emit('new-message',payload);
--- server/main.js
+++ server/main.js
@@ -1,32 +1,87 @@
 var express = require('express');
+var jf = require("jsonfile");
 var app = express();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
-var messages =[{
-    id: 1,
-    text: "Hola soy mensaje",
-    autor: "Sebastian O"
-}]
+
+var estructura;
+var jsonPath = __dirname + '/estructura.json';
+
+function writeJSONFile( estructura){
+    return new Promise((resolve, reject) => {
+        jf.writeFile(jsonPath, estructura, function (err) {
+            try {
+                resolve()
+            } catch (err) {
+                reject(err)
+            }
+        });
+    });
+}
+
+/*readJSONFile("")
+.then(function(ll){
+    var li = ll;
+})
+.catch()*/
+
+function readJSONFile(jsonPath){
+    return new Promise((resolve, reject) => {
+        jf.readFile(jsonPath, function(err, estructura) {
+            try {
+                resolve(estructura);
+            } catch (err) {
+                reject(err);
+            }
+        })
+    });
+}
+
+var messages = [];
+// =[{
+//     id: 1,
+//     text: "Hola soy mensaje",
+//     author: "Sebastian O"
+
+// }]
 
 app.use(express.static('public'));
 
 app.get('/', function(req, res){
     res.status(200).send("hola mundo");
- 
 });
   
 io.on('connection', function(socket){
     
     console.log("Alguien se ha conectado!");
-    socket.emit('messages', messages);
-    socket.on('new-message', function(data){
-        messages.push(data);
-    }); 
+    
+    readJSONFile(jsonPath)
+    .then(estructuraJSON => {
+       // estructuraJSON.eventos[0]=""; 
+        //writeJSONFile(estructuraJSON); 
+        
+       messages.push(estructuraJSON);
+        //console.log(messages);
+    })
+    .then(noCare => {
+       // console.log(messages);
+        
+            //console.log(messages[i]);
+            socket.emit('messages', messages);
+        
+        socket.on('new-message', function(data){
+            messages.push(data);
+            io.sockets.emit('messages', messages);// avisa a todos los sockets sobre el mensaje
+        }); 
+    })
+    .catch(error => {
+        console.log("error");
+    })
 
 });      
  
-server.listen(8084, function(){
 
+server.listen(8084, function(){
     console.log("servidor corriendo  8084")
 });         
  
