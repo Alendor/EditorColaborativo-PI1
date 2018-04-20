f30a0b7a49ab124c25e3bddb4e373fdd858c8543
Sebastian Osorno Zapata <osorno.sebastian@gmail.com>
2018-04-20 02:39:26 -0500

    angular con json

---
+++ Frontend/public-editor/src/app/app.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import * as socketIo from 'socket.io-client';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+  title = 'app';
+  text = "";
+  text2 = "";
+  
+ 
+  
+  ngOnInit(): void {
+    const socket = socketIo('http://localhost:5000');
+    //socket.on('hello', (data) => console.log(data));
+    socket.on('messages', (data) =>  {
+      this.text = JSON.stringify(data);
+      
+      this.text2 = data;
+    console.log(data);
+    
+    });
+   
+  }
+
+
+  
+}
